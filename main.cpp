d8f5190fc223fc6884e64b918d75fcd08a781151
springc2 <springc2@tcnj.edu>
2015-02-17 16:46:44 -0500

    Added loop in main to perform all 32 simulations

--- main.cpp
+++ main.cpp
@@ -21,82 +21,285 @@ int binaryStringToDecimal(string binary);
 
 int main(int argc, const char * argv[])
 {
-    int length;
-    
-    //for Sim 1 only, will have to perform all simulations
-    int K = 2;  // Num Lines per Set
-    int N = 32; //Num Sets
-    int hitCount = 0;
-    int missCount = 0;
-    bool isFIFO = false;
-    
-    ifstream inFile;
-    inFile.open ("TRACE1.DAT" , ifstream::binary);
-    
-    if(inFile.fail())
+    int K;  // Num Lines per Set
+    int N; //Num Sets
+    string filename;
+    bool isFIFO;
+    for (int sim = 0; sim < 32; sim++)
     {
-        cerr << "Failed to read from file TRACE1.DAT" << endl;
-    }
-    
-    inFile.seekg (0, ios::end);
-    length = (int)inFile.tellg();
-    inFile.seekg (0, ios::beg);
-    
-    char data[length];
-    
-    inFile.read(data, length);
-    inFile.close();
-    
-    vector<string> memRefs(length/3);
-    int countMemRefs = 0;
-    for (int i = 0; i < length; i++)
-    {
-        stringstream ss;
-        ss << hex << setw(2) << setfill('0') << (int)(unsigned char)data[i];
-        memRefs[i/3].insert(0,ss.str());
-        
-        if(i%3 == 2)
+        switch (sim)
         {
-            //  cout << memRefs[i/3] << endl;
-            countMemRefs++;
+            case 0:
+                K = 2;
+                N = 32;
+                isFIFO = true;
+                filename = "TRACE1.DAT";
+                break;
+            case 1:
+                K = 4;
+                N = 16;
+                isFIFO = true;
+                filename = "TRACE1.DAT";
+                break;
+            case 2:
+                K = 8;
+                N = 8;
+                isFIFO = true;
+                filename = "TRACE1.DAT";
+                break;
+            case 3:
+                K = 16;
+                N = 4;
+                isFIFO = true;
+                filename = "TRACE1.DAT";
+                break;
+            case 4:
+                K = 2;
+                N = 128;
+                isFIFO = true;
+                filename = "TRACE1.DAT";
+                break;
+            case 5:
+                K = 4;
+                N = 64;
+                isFIFO = true;
+                filename = "TRACE1.DAT";
+                break;
+            case 6:
+                K = 8;
+                N = 32;
+                isFIFO = true;
+                filename = "TRACE1.DAT";
+                break;
+            case 7:
+                K = 16;
+                N = 16;
+                isFIFO = true;
+                filename = "TRACE1.DAT";
+                break;
+            case 8:
+                K = 2;
+                N = 32;
+                isFIFO = false;
+                filename = "TRACE1.DAT";
+                break;
+            case 9:
+                K = 4;
+                N = 16;
+                isFIFO = false;
+                filename = "TRACE1.DAT";
+                break;
+            case 10:
+                K = 8;
+                N = 8;
+                isFIFO = false;
+                filename = "TRACE1.DAT";
+                break;
+            case 11:
+                K = 16;
+                N = 4;
+                isFIFO = false;
+                filename = "TRACE1.DAT";
+                break;
+            case 12:
+                K = 2;
+                N = 128;
+                isFIFO = false;
+                filename = "TRACE1.DAT";
+                break;
+            case 13:
+                K = 4;
+                N = 64;
+                isFIFO = false;
+                filename = "TRACE1.DAT";
+                break;
+            case 14:
+                K = 8;
+                N = 32;
+                isFIFO = false;
+                filename = "TRACE1.DAT";
+                break;
+            case 15:
+                K = 16;
+                N = 16;
+                isFIFO = false;
+                filename = "TRACE1.DAT";
+                break;
+            case 16:
+                K = 2;
+                N = 32;
+                isFIFO = true;
+                filename = "TRACE2.DAT";
+                break;
+            case 17:
+                K = 4;
+                N = 16;
+                isFIFO = true;
+                filename = "TRACE2.DAT";
+                break;
+            case 18:
+                K = 8;
+                N = 8;
+                isFIFO = true;
+                filename = "TRACE2.DAT";
+                break;
+            case 19:
+                K = 16;
+                N = 4;
+                isFIFO = true;
+                filename = "TRACE2.DAT";
+                break;
+            case 20:
+                K = 2;
+                N = 128;
+                isFIFO = true;
+                filename = "TRACE2.DAT";
+                break;
+            case 21:
+                K = 4;
+                N = 64;
+                isFIFO = true;
+                filename = "TRACE2.DAT";
+                break;
+            case 22:
+                K = 8;
+                N = 32;
+                isFIFO = true;
+                filename = "TRACE2.DAT";
+                break;
+            case 23:
+                K = 16;
+                N = 16;
+                isFIFO = true;
+                filename = "TRACE2.DAT";
+                break;
+            case 24:
+                K = 2;
+                N = 32;
+                isFIFO = false;
+                filename = "TRACE2.DAT";
+                break;
+            case 25:
+                K = 4;
+                N = 16;
+                isFIFO = false;
+                filename = "TRACE2.DAT";
+                break;
+            case 26:
+                K = 8;
+                N = 8;
+                isFIFO = false;
+                filename = "TRACE2.DAT";
+                break;
+            case 27:
+                K = 16;
+                N = 4;
+                isFIFO = false;
+                filename = "TRACE2.DAT";
+                break;
+            case 28:
+                K = 2;
+                N = 128;
+                isFIFO = false;
+                filename = "TRACE2.DAT";
+                break;
+            case 29:
+                K = 4;
+                N = 64;
+                isFIFO = false;
+                filename = "TRACE2.DAT";
+                break;
+            case 30:
+                K = 8;
+                N = 32;
+                isFIFO = false;
+                filename = "TRACE2.DAT";
+                break;
+            case 31:
+                K = 16;
+                N = 16;
+                isFIFO = false;
+                filename = "TRACE2.DAT";
+                break;
+            default:
+                break;
         }
-    }
-    
-    vector<string> binaryMemRefs(length/3);
-    
-    for (int i = 0; i < length/3; i++)
-    {
-        binaryMemRefs[i] = hexStringToBinaryString(memRefs[i]);
-    }
-    
-    //less than 60,000?
-    cout << "Num memrefs is " << countMemRefs << endl;
-    
-    //creates array of Queues for each line of cache
-    CacheSetQueue* cacheLines = new CacheSetQueue[K];
-    for (int i = 0; i < K; i++)
-    {
-        cacheLines[i] = CacheSetQueue(N, isFIFO);
-    }
-    
-    int numIndexBits = log2(K);  //calculate number of index bits
-    for (int i = 0; i < length/3; i++)
-    {
-        int lineIndex = binaryStringToDecimal(binaryMemRefs[i].substr(20-numIndexBits, numIndexBits));  //extract index from address
-        string tag = binaryMemRefs[i].substr(0, 20-numIndexBits);  //extract tag from address
+        int length;
+        int hitCount = 0;
+        int missCount = 0;
         
-        if(cacheLines[lineIndex].addToQueue(tag))  //
-            hitCount++;
-        else
-            missCount++;
+        ifstream inFile;
+        inFile.open (filename , ifstream::binary);
+        
+        if(inFile.fail())
+        {
+            cerr << "Failed to read from file " << filename << endl;
+        }
+        
+        inFile.seekg (0, ios::end);
+        length = (int)inFile.tellg();
+        inFile.seekg (0, ios::beg);
+        
+        char data[length];
+        
+        inFile.read(data, length);
+        inFile.close();
+        
+        vector<string> memRefs(length/3);
+        int countMemRefs = 0;
+        for (int i = 0; i < length; i++)
+        {
+            stringstream ss;
+            ss << hex << setw(2) << setfill('0') << (int)(unsigned char)data[i];
+            memRefs[i/3].insert(0,ss.str());
+            
+            if(i%3 == 2)
+            {
+                //  cout << memRefs[i/3] << endl;
+                countMemRefs++;
+            }
+        }
+        
+        vector<string> binaryMemRefs(length/3);
+        
+        for (int i = 0; i < length/3; i++)
+        {
+            binaryMemRefs[i] = hexStringToBinaryString(memRefs[i]);
+        }
+        
+        //less than 60,000?
+        cout << "\n\nSimulation " << sim << " with " << filename << endl;
+        cout << "Num memrefs is " << countMemRefs << endl;
+        
+        //creates array of Queues for each line of cache
+        CacheSetQueue* cacheLines = new CacheSetQueue[K];
+        for (int i = 0; i < K; i++)
+        {
+            cacheLines[i] = CacheSetQueue(N, isFIFO);
+        }
+        
+        int numIndexBits = log2(K);  //calculate number of index bits
+        for (int i = 0; i < length/3; i++)
+        {
+            int lineIndex = binaryStringToDecimal(binaryMemRefs[i].substr(20-numIndexBits, numIndexBits));  //extract index from address
+            string tag = binaryMemRefs[i].substr(0, 20-numIndexBits);  //extract tag from address
+            
+            if(cacheLines[lineIndex].addToQueue(tag))  //
+                hitCount++;
+            else
+                missCount++;
+        }
+        
+        /*for (int i = 0; i < cacheLines[1].getSize(); i++)
+        {
+            cout << cacheLines[1].getTag(i) << endl;
+        }*/
+        cout << "Number of Sets: " << N << endl;
+        cout << "Number of Lines per Set: " << K << endl;
+        cout << "Number of Misses: " << missCount << endl;
+        cout << "Number of Hits: " << hitCount << endl;
+        
     }
-    
-    /*for (int i = 0; i < cacheLines[1].getSize(); i++)
-    {
-        cout << cacheLines[1].getTag(i) << endl;
-    }*/
-    cout << "Num Misses: " << missCount << endl;
-    cout << "Num Hits: " << hitCount << endl;
     
     return 0;
 }
